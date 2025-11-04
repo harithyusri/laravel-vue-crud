80c3bdfd5d2990cf5030e7ae0c96c6c445ec1415
Harith Yusri <mharith0712@gmail.com>
2025-11-04 22:37:40 +0800

    Add Product sidebar and route

--- resources/js/components/AppSidebar.vue
+++ resources/js/components/AppSidebar.vue
@@ -14,14 +14,19 @@ import {
 import { dashboard } from '@/routes';
 import { type NavItem } from '@/types';
 import { Link } from '@inertiajs/vue3';
-import { BookOpen, Folder, LayoutGrid } from 'lucide-vue-next';
+import { BookOpen, Folder, LayoutGrid, PackageSearch } from 'lucide-vue-next';
 import AppLogo from './AppLogo.vue';
 
 const mainNavItems: NavItem[] = [
     {
         title: 'Dashboard',
-        href: dashboard(),
+        href: '/dashboard',
         icon: LayoutGrid,
+    },
+    {
+        title: 'Products',
+        href: '/products',
+        icon: PackageSearch,
     },
 ];
 
--- routes/web.php
+++ routes/web.php
@@ -14,4 +14,8 @@ Route::get('dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::middleware(['auth', 'verified'])->group(function(){
+    Route::get('/products',[])->name('products.index');
+});
+
 require __DIR__.'/settings.php';
